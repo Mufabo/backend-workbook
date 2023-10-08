3bfa5627c8b4bbc10180af23c7860c55c53f65a5
mufabo <fatbos1206@googlemail.com>
2023-10-08 14:38:52 +0200

    01 05 template

--- 01/_01_05_greeting/src/test/java/wepa/_01_05_greeting/ApplicationTests.java
+++ 01/_01_05_greeting/src/test/java/wepa/_01_05_greeting/ApplicationTests.java
@@ -1,13 +1,39 @@
 package wepa._01_05_greeting;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.net.HttpURLConnection;
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void test1() throws Exception {
+		this.mockMvc.perform(get("/greet?greeting=Greetings&name=Earthling")).andExpect(content().string("Greetings, Earthling"));
+	}
+
+	@Test
+	public void paramTest2() throws Exception {
+		this.mockMvc.perform(get("/greet?greeting=Oi&name=Mate")).andExpect(content().string("Oi, mate"));
+	}
+
+	@Test
+	public void randomPathTest() throws Exception {
+		this.mockMvc.perform(get("/" + UUID.randomUUID().toString())).andExpect(status().is(HttpURLConnection.HTTP_NOT_FOUND));
 	}
 
 }
